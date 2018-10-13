79005e6780cce23a5944b1c2b14dfd24031d17c7
Alex Masterov <alex.masterow@gmail.com>
2018-10-13 10:40:13 +0300

    Improve Decoder tests

--- test/Decoder.test.js
+++ test/Decoder.test.js
@@ -1,105 +1,158 @@
 'use strict';
 
 const assert = require('assert');
-const { Decoder, errors: { DecodingFailed, InsufficientData } } = require('../src');
+const stub = require('./stub');
 
-const types = require('./stub');
+const Decoder = require('../src/Decoder');
+const DecodingFailed = require('../src/errors/DecodingFailed');
+const InsufficientData = require('../src/errors/InsufficientData');
 
 // assets module v8.x can't compare NaN and exceptions
 const isNode8 = process.version[1] === '8';
+const assertDeepStrictEqualNode8 = (actual, expected) =>
+  isNode8 && Number.isNaN(expected)
+    ? assert.ok(Number.isNaN(actual))
+    : assert.deepStrictEqual(actual, expected);
 
-function testDecode(type) {
-  type.forEach(({ name, value: expected, bin }) => {
-    const decoder = new Decoder();
-    it(name, () => {
-      const buffer = Buffer.from(bin);
-      const actual = decoder.decode(buffer);
+const testStub = (name, stub) => process =>
+  describe(name, () => stub.forEach(({ name, value, bin: buffer }) =>
+    it(name, () => process(buffer, value))
+  ));
 
-      assert.deepStrictEqual(actual, expected);
-    });
-  });
-}
+const test = (...stubs) => process =>
+  stubs.forEach(type => testStub(type, stub[type])(process));
 
-function testThrowsNoData(buffer) {
-  it(`throws when no data`, () => {
-    const decoder = new Decoder();
-    assert.throws(() => decoder.decode(buffer), {
-      name: 'DecodingFailed',
-      message: 'No data to decode',
-    });
-  });
-}
-
-function testThrowsDecodingFailed(byte) {
-  it(`throws when decode unknown byte (${byte})`, () => {
-    const decoder = new Decoder();
-    assert.throws(() => decoder.decode(byte), {
-      name: 'DecodingFailed',
-    });
-  });
-}
-
-function testThrowsInsufficientData(name, buffer, actualLength, expectedLength) {
-  it(`throws when ${name} data is insufficien`, () => {
-    const decoder = new Decoder();
-    assert.throws(() => decoder.decode(buffer), {
-      name: 'InsufficientData',
-      message: `Not enough data to decode: expected length ${expectedLength}, got ${actualLength}`,
-    });
-  });
-}
+const testUnexpectedLength = (...stubs) => process =>
+  stubs.forEach(([type, slice]) =>
+    describe(type, () => stub[type].forEach(({ name, bin: buffer }) =>
+      it(name, () => process(buffer.slice(0, slice)))
+    )));
 
 describe('Decoder', () => {
-  if (isNode8) {
-    const excludeNaN = type => type.name !== 'NaN';
-    types.float32 = types.float32.filter(excludeNaN);
-    types.float64 = types.float64.filter(excludeNaN);
-  }
+  test('nil')((buffer, expected) => {
+    const decoder = new Decoder();
+    assert.deepStrictEqual(decoder.decode(buffer), expected);
+  });
 
-  const excludeOverflow = type => !type.name.includes('overflow');
-  types.uint64 = types.uint64.filter(excludeOverflow);
-  types.int64 = types.int64.filter(excludeOverflow);
+  test('boolean')((buffer, expected) => {
+    const decoder = new Decoder();
+    assert.deepStrictEqual(decoder.decode(buffer), expected);
+  });
 
-  const skip = [
-    'bigint64',
-    'fixmap',
-    'map16',
-    'map32',
-  ];
+  test('float32')((buffer, expected) => {
+    const decoder = new Decoder();
+    assertDeepStrictEqualNode8(decoder.decode(buffer), expected);
+  });
 
-  const tests = Object.entries(types)
-    .filter(([name]) => skip.indexOf(name) === -1);
+  test('float64')((buffer, expected) => {
+    const decoder = new Decoder();
+    assertDeepStrictEqualNode8(decoder.decode(buffer), expected);
+  });
 
-  for (const [name, type] of tests) {
-    describe(name, () => testDecode(type));
-  }
+  test(
+    'fixint',
+    'uint8',
+    'uint16',
+    'uint32',
+    'uint64',
+    '-fixint',
+    'int8',
+    'int16',
+    'int32',
+    'int64',
+  )((buffer, expected) => {
+    const decoder = new Decoder();
+    assert.deepStrictEqual(decoder.decode(buffer), expected);
+  });
 
-  if (isNode8) return;
+  test(
+    'fixstr',
+    'str8',
+    'str16',
+    'str32',
+    'utf8',
+  )((buffer, expected) => {
+    const decoder = new Decoder({ bufferMinLen: 6 });
+    assert.deepStrictEqual(decoder.decode(buffer), expected);
+  });
 
-  const bytes = (...bytes) => Buffer.from(bytes);
+  test(
+    'bin8',
+    'bin16',
+    'bin32',
+  )((buffer, expected) => {
+    const decoder = new Decoder();
+    assert.deepStrictEqual(decoder.decode(buffer), expected);
+  });
 
-  testThrowsNoData(bytes());
-  testThrowsDecodingFailed('\xc1');
-  testThrowsInsufficientData('float32', bytes(0xca), 0, 4);
-  testThrowsInsufficientData('float64', bytes(0xcb), 0, 8);
-  testThrowsInsufficientData('int8', bytes(0xd0), 0, 1);
-  testThrowsInsufficientData('int16', bytes(0xd1), 0, 2);
-  testThrowsInsufficientData('int32', bytes(0xd2), 0, 4);
-  testThrowsInsufficientData('int64', bytes(0xd3), 0, 8);
-  testThrowsInsufficientData('uint8', bytes(0xcc), 0, 1);
-  testThrowsInsufficientData('uint16', bytes(0xcd), 0, 2);
-  testThrowsInsufficientData('uint32', bytes(0xce), 0, 4);
-  testThrowsInsufficientData('uint64', bytes(0xcf), 0, 8);
-  testThrowsInsufficientData('str', bytes(0xa1), 0, 1);
-  testThrowsInsufficientData('bin8', bytes(0xc4), 0, 1);
-  testThrowsInsufficientData('bin16', bytes(0xc5), 0, 2);
-  testThrowsInsufficientData('bin32', bytes(0xc6), 0, 4);
-  testThrowsInsufficientData('bin8 (data)', bytes(0xc4, 0x20), 0, 32);
-  testThrowsInsufficientData('bin16 (data)', bytes(0xc5, 0x01, 0x00), 0, 256);
-  testThrowsInsufficientData('bin32 (data)', bytes(0xc6, 0x00, 0x01, 0x00, 0x00), 0, 65536);
-  // testThrowsInsufficientData('fixext1', bytes(0xd4), 0, 1);
-  // testThrowsInsufficientData('fixext2', bytes(0xd5), 0, 2);
-  // testThrowsInsufficientData('fixext4', bytes(0xd6), 0, 4);
-  // testThrowsInsufficientData('fixext8', bytes(0xd7), 0, 8);
-  // testThrowsInsufficientData('fixext16', bytes(0xd8), 0, 16);
+  test(
+    'fixarr',
+    'arr16',
+    'arr32',
+  )((buffer, expected) => {
+    const decoder = new Decoder();
+    assert.deepStrictEqual(decoder.decode(buffer), expected);
+  });
+
+  test(
+    'fixobj',
+    'obj16',
+    'obj32',
+  )((buffer, expected) => {
+    const decoder = new Decoder();
+    assert.deepStrictEqual(decoder.decode(buffer), expected);
+  });
+
+  test(
+    'fixext',
+    'ext8',
+    'ext16',
+    'ext32',
+  )((buffer, expected) => {
+    const decoder = new Decoder();
+    assert.deepStrictEqual(decoder.decode(buffer), expected);
+  });
+
+  describe('throws', () => {
+    it('no data', () => {
+      const decoder = new Decoder();
+      const buffer = Buffer.allocUnsafe(0);
+      assert.throws(() => decoder.decode(buffer), DecodingFailed);
+    });
+
+    it('decode unknown byte (0xc1)', () => {
+      const decoder = new Decoder();
+      const buffer = Buffer.from([0xc1]);
+      assert.throws(() => decoder.decode(buffer), DecodingFailed);
+    });
+
+    testUnexpectedLength(
+      ['float32', 1],
+      ['float64', 1],
+      ['int8', 1],
+      ['int16', 1],
+      ['int32', 1],
+      ['int64', 1],
+      ['uint8', 1],
+      ['uint16', 1],
+      ['uint32', 1],
+      ['uint64', 1],
+      ['str8', 1],
+      ['str16', 3],
+      ['str32', 5],
+      ['bin8', 1],
+      ['bin16', 3],
+      ['arr16', 2],
+      ['arr32', 4],
+      ['obj16', 2],
+      ['obj32', 4],
+      ['fixext', 1],
+      ['ext8', 3],
+      ['ext16', 4],
+      ['ext32', 6],
+    )(buffer => {
+      const decoder = new Decoder();
+      assert.throws(() => decoder.decode(buffer), InsufficientData);
+    });
+  });
 });
