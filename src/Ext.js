0de6952a8c267af1e529c10a35d0829576fb85a5
Alex Masterov <alex.masterow@gmail.com>
2018-11-09 12:20:23 +0300

    Remove static constructor from Ext

--- src/Ext.js
+++ src/Ext.js
@@ -5,13 +5,9 @@ function required(name) {
 }
 
 class Ext {
-  static make(type, bin) {
-    return new this(type, bin);
-  }
-
   constructor(
-    type = required('type'),
-    bin = required('bin')
+    type=required('type'),
+    bin=required('bin')
   ) {
     this.type = type;
     this.bin = bin;
