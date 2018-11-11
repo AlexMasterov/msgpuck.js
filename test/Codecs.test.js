00683d7968449c838480acce0c9badc817aa28b5
Alex Masterov <alex.masterow@gmail.com>
2018-11-11 19:26:00 +0300

    Improve test codecs

--- test/Codecs.test.js
+++ test/Codecs.test.js
@@ -25,17 +25,26 @@ function process(value, ...codecs) {
 describe('Codecs', () => {
   describe('[Abstract] Codec', () => {
     it(`Default codec type (${Codec.type})`, () => {
-      const actual = class extends Codec {};
-      assert.deepStrictEqual(actual.type, Codec.type);
+      const UselessCodec = class extends Codec {
+        encode(encoder, value) { return null; }
+      };
+
+      const uselessCodec = new UselessCodec(42);
+      assert.deepStrictEqual(UselessCodec.type, Codec.type);
+      assert.deepStrictEqual(uselessCodec.type, 42);
+
+      const actual = process({}, uselessCodec);
+      assert.deepStrictEqual(actual, {});
     });
   });
 
   describe('ScalarObjectCodec', () => {
     test(
-      type('Number', Number(42), 42),
-      type('String', String('xyz'), 'xyz'),
-      type('Boolean (true)', Boolean(true), true),
-      type('Boolean (false)', Boolean(false), false),
+      type('Number', new Number(42), 42),
+      type('String', new String('xyz'), 'xyz'),
+      type('Boolean (true)', new Boolean(true), true),
+      type('Boolean (false)', new Boolean(false), false),
+      type('default', { default: true }, { default: true }),
     )((value, expected) => {
       const actual = process(value, new ScalarObjectCodec());
       assert.deepStrictEqual(actual, expected);
